7b9b1ea26f2dbbdeb5aa1af133f2efde8d92c5f5
Your Name <you@example.com>
2023-09-17 15:12:46 +1000

    cp

---
+++ DB2/scripts/runme.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+cat << 'EOF' > /tmp/create_table.sql
+connect to sample ;
+
+drop table cars ;
+
+CREATE TABLE CARS (
+      id integer not null GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) ,
+      make     char(20) ,
+      model    char(20) ,
+      year     int ,
+      price    decimal(7,2)
+);
+
+insert into CARS ( make , model , year , price ) values ( 'Toyota' , 'Prius' , 2020 , 24000 ) ;
+insert into CARS ( make , model , year , price ) values ( 'Toyota' , 'Avensis' , 2013, 12000) ;
+insert into CARS ( make , model , year , price ) values ( 'Toyota' , 'Camry' , 2022, 25000) ;
+
+insert into CARS ( make , model , year , price ) values ( 'Honda' , 'Accord' , 2022, 25000) ;
+
+insert into CARS ( make , model , year , price ) values ( 'Nissan' , 'Xtrail' , 2022, 25000) ;
+EOF
+
+cat <<EOF > /tmp/doit.sh
+echo Output of runme
+
+db2pd -
+
+
+db2stop
+
+db2set DB2COMM=SSL,TCPIP
+
+db2start
+
+
+db2 -tvf /tmp/create_table.sql
+EOF
+
+
+
+chmod +x /tmp/doit.sh
+
+su - db2inst1 -c /tmp/doit.sh
+
+
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Airflow-Testenv
-Test Environment
+
+
+
+#  Setting up the Test Env
+
+
+Repo : http://shiga:3000/quekch/Kinisi-TestEnv.git
+
+Development :
+
+- carmen /test/env:/Kinisi-TestEnv
+- cd /testenv/Kinisi-Testenv/DB2
+- tar cvf data.tar data
+- gzip data.tar
+- scp data.tar.gz root@cuclone.kinisi.biz:/root/Kinisi-TestEnv/DB2
+
+Production :
+
+- cuclone /root/Kinisi-TestEnv
+- tar cvf DB2/data folder and import it here.
+- docker-compose -f docker-compose.clone.yaml up
+
+
+This step is to avoid re-generating certificates and have to amend the datasource
